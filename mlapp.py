2b021650d32e272eac4786aa118dd4b5621089af
RimaHinn <55820929+RimaHinn@users.noreply.github.com>
2021-01-15 18:55:22 -0500

    updated file
    x
    :wq

--- mlapp.py
+++ mlapp.py
@@ -5,9 +5,12 @@ import pickle
 #use the model that was created in DiabetesDetection.ipynb 
 model = pickle.load(open("diabetes_random_forrest_model.pkl", 'rb'))
 
-
+ 
+st.set_page_config(page_title="AI HEALTH TOOLS", page_icon=None)
+                   
 #function for getting user input through streamlit app
 def user_input():
+   
     pregnancies = st.sidebar.slider("Pregancies",0,10) 
     glucose = st.sidebar.slider("Glucose",0,200) 
     blood_pressure = st.sidebar.slider("Blood Pressure",0,122) 
@@ -41,50 +44,60 @@ def predict_diabetes(user_data):
 def main():
 
     #HTML code variables
-    html_temp = """
-    <div style="background:#666060;padding:30px">
-    <h2 style="color:white;text-align:center;"> Diabetes Preciction ML App </h2>
+    html_main_header = """
+    <div style="background:#000000;padding:10px">
+    <h2 style="color:white;text-align:center;"> AI Health Tools </h2>
     </div>
     """
-   
+    # sub header: Diabetes Preciction ML App
     diabetic_html = """
-    <div style="background:#d63636 ;padding:2px">
+    <div style="background:#ed7834 ;padding:1px">
     <h3 style="color:white;text-align:center;"> Patient is Diabetic</h3>
     </div>
     """
     
     not_diabetic_html = """
-    <div style="background:#a3b591 ;padding:2px">
+    <div style="background:#5c92bf ;padding:1px">
     <h3 style="color:white;text-align:center;"> Patient is not Diabetic</h3>
     </div>
     """
-  
     
     # Main Header 
-    st.markdown(html_temp, unsafe_allow_html = True)
+
+    st.markdown(html_main_header, unsafe_allow_html = True)
 
     # Side Bar Header
-    st.sidebar.header('Select Patient Data')
+    st.sidebar.image('https://venturebeat.com/wp-content/uploads/2020/11/image001.png', use_column_width= True)
+        
+    #menu item selcction
+    
+    menu_items= [" ", "Diabetes Predictor", "Pneumonia Predictor",
+                 "COVID-19 Symptom Checker", "Heart Disease Predictor", "Eye Disease Predictor"]
+    
+    choice = st.sidebar.selectbox("MENU", menu_items)
     
     
-    # PATIENT DATA
-    user_data = user_input()
-    st.subheader('User Input Data')
-    st.write(user_data)
+    if choice == "Diabetes Predictor":
+        
+        # PATIENT DATA
+        st.subheader('DIABETES PREDICTOR')
+        st.write("User Input:")
+        user_data = user_input()
+        st.write(user_data)
              
-    #ML function called and result determined and displayed
-    if st.button("Predict"): 
-        pred=predict_diabetes(user_data)
+        #ML function called and result determined and displayed
+        if st.button("Predict"): 
+            pred=predict_diabetes(user_data)
         
-        if pred == 1:
-            #Patient is Diabetic - write html code with patient is diabetic
-            st.markdown(diabetic_html, unsafe_allow_html = True)
-            #st.write("Prediction Probability:", model.predict_proba(user_data))
+            if pred == 1:
+                #Patient is Diabetic - write html code with patient is diabetic
+                st.markdown(diabetic_html, unsafe_allow_html = True)
+                #st.write("Prediction Probability:", model.predict_proba(user_data))
         
-        else:
-            #Patient is Diabetic - write html code with patient is not diabetic
-            st.markdown(not_diabetic_html, unsafe_allow_html = True)
-            #st.write("Prediction Probability:", model.predict_proba(user_data))
+            else:
+                #Patient is Diabetic - write html code with patient is not diabetic
+                st.markdown(not_diabetic_html, unsafe_allow_html = True)
+                #st.write("Prediction Probability:", model.predict_proba(user_data))
             
    
     
